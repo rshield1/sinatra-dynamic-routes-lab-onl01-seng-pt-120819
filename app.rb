0a3ee691bbdff7315a2a40e31a80ee4d0878a933
Robert Shields <robert.shieldsiv@gmail.com>
2020-03-20 02:12:19 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,5 +2,11 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+  get '/reversename/:name' do
+    params[:name].reverse
+  end
+  get '/say/:number/:phrase' do 
+    params[:number]
+
 
 end
